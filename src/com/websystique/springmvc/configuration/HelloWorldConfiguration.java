8879a1de8b2788e6f6c9aa9c237478d66ab8ab62
weiyang <tw00182691@gmail.com>
2017-04-05 08:06:05 +0800

    解決url pattern 問題

---
+++ src/com/websystique/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,32 @@
+package com.websystique.springmvc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com")
+public class HelloWorldConfiguration extends WebMvcConfigurerAdapter{
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		registry.viewResolver(viewResolver);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/myApp/assets/lib/**").addResourceLocations("/myApp/assets/lib/");
+		registry.addResourceHandler("/myApp/assets/js/**").addResourceLocations("/myApp/assets/js/");
+	}
+
+}
